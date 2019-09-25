131363c1daa109683d3ea5de4f9aff3fc66bab93
Egil <egil.moller@freecode.no>
2019-09-25 11:10:42 +0200

    Test debug prints

--- screen/firmware/test/test.py
+++ screen/firmware/test/test.py
@@ -15,6 +15,10 @@ def wr(s):
     sys.stdout.write(s)
     sys.stdout.flush()
 
+def dbg(s):
+    sys.stderr.write(s + "\n")
+    sys.stderr.flush()
+    
 # Reset screen
 wr("\x1bc")
 
@@ -24,45 +28,58 @@ wr("""\
 *                              *
 *                              *
 *                              *
+* Print screen                 *
+* Exit                         *
 ********************************
 """)
 
-wr("\x1b[5A\x1b[2COn the first line")
+wr("\x1b[7A\x1b[2COn the first line")
 wr("\x1b[1E\x1b[3GOn the second line")
 wr("\x1b[4;3HOn the muahahaha")
 wr("\x1b[1E\x1b[2COn the forth line")
 wr("\x1b[5;4H")
 wr("\x1b[1A\x1b[1DOn the third line")
-wr("\x1b[7;1H")
 
 wr("\x1b[2;2H")
 
-wr("\x1b[0i")
-
+pos = 2
 while True:
     c1 = rd()
-    #if c1 == "x":
-    #    termios.tcsetattr(stdinfd, termios.TCSANOW, old_settings)
-    #    break
     if c1 == "\x1b":
+        dbg("ESC")
         c2 = rd()
-
+        
         if c2 == "[":
+            dbg("CSI")
             c3 = rd()
       
             if c3 == "A":
+                dbg("CSI UP")
+                pos -= 1
                 wr("\x1b[2G ")
                 wr("\x1b[A\x1b[2GX")
             elif c3 == "B":
+                dbg("CSI DOWN")
+                pos += 1
                 wr("\x1b[2G ")
                 wr("\x1b[B\x1b[2GX")
             else:
-                print("c3", repr(c3))
+                dbg("Unknown CSI " + c3)
         else:
-            print("c2", repr(c2))
-    #elif c1 == "\n" or c1 == "\r":
-    #    wr(">>>>>>>>>>>>>>>>>>>>>>>>>>>>")
-    #    wr("\x1b[2G>")
+            dbg("Unknown ESC " + c2)
+    elif c1 == "\n" or c1 == "\r":
+        dbg("ENTER")
+        if pos == 6:
+            dbg("PRINT SCREEN")
+            wr("\x1b[0i")
+        elif pos == 7:
+            dbg("EXIT")
+            termios.tcsetattr(stdinfd, termios.TCSANOW, old_settings)
+            break
+        else:
+            dbg("SELECT")
+            wr(">>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+            wr("\x1b[2G>")
     else:
         sys.stderr.write(c1)
         sys.stderr.flush()
