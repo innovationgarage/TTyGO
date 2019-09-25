9b5de7e511aed002e8057dfecf0a321d9114fdfd
Erwin Ried <1091420+eried@users.noreply.github.com>
2019-09-25 10:53:05 +0200

    Using the most popular OneButton library

--- screen/firmware/TTGOTerm/Buttons.ino
+++ screen/firmware/TTGOTerm/Buttons.ino
@@ -1,47 +1,47 @@
+OneButton button_left(BTN_LEFT, true), button_middle(BTN_MID, true), button_right(BTN_RIGHT, true);
+
+void button_send_key(char c)
+{
+  Serial.print(ESC);
+  Serial.print(CSI);
+  Serial.print(c);
+}
+
+// Cursor up
+void button_left_click()
+{
+  button_send_key('A');
+}
+
+// Enter
+void button_middle_click()
+{
+  Serial.print(CR);
+}
+
+// Cursor down
+void button_right_click()
+{
+  button_send_key('B');
+}
+
 // Task for the buttons
 class KeyboardTask : public Task {
   protected:
     void setup()
     {
-      // Set buttons (all supported modes: https://github.com/TanPitch/ButtonKing/blob/master/examples/ButtonQueen/ButtonQueen.ino )
-      /*button_left.setClick(button_left_click);
-      button_middle.setClick(button_middle_click);
-      button_right.setClick(button_right_click);*/
+      // Set buttons (all supported modes: https://github.com/mathertel/OneButton/blob/master/examples/TwoButtons/TwoButtons.ino )
+      button_left.attachClick(button_left_click);
+      button_middle.attachClick(button_middle_click);
+      button_right.attachClick(button_right_click);
     }
 
-    void button_send_key(char c)
-    {
-      Serial.print(ESC);
-      Serial.print(CSI);
-      Serial.print(c);
-    }
-
-    // Cursor up
-    void button_left_click()
-    {
-      button_send_key('A');
-    }
-
-    // Enter
-    void button_middle_click()
-    {
-      Serial.print(CR);
-    }
-
-    // Cursor down
-    void button_right_click()
-    {
-      button_send_key('B');
-    }
     void loop()
     {
       // Check buttons
-      button_left.isClick();
-      button_middle.isClick();
-      button_right.isClick();
+      button_left.tick();
+      button_middle.tick();
+      button_right.tick();
+      delay(10);
     }
-
-  private:
-/* */
-
 } keyboard_task;
