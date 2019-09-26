dea52ed045591d2743f639b5692e0e9c96dde7b9
Egil <egil.moller@freecode.no>
2019-09-26 15:27:43 +0200

    Test bugfixes

--- screen/firmware/test/automated/test_csi.py
+++ screen/firmware/test/automated/test_csi.py
@@ -38,6 +38,13 @@ class TestCSI(unittest.TestCase):
 """
 
     linelen = len(orig.split("\n")[0])
+
+    def assertScreen(self, shouldbe, actual):
+        def clean(s):
+            return "\n".join(l.rstrip() for l in s.rstrip().split("\n"))
+        shouldbe = clean(shouldbe)
+        actual = clean(actual)
+        self.assertEqual(shouldbe, actual, "\n%s\n!=\n%s" % (shouldbe, actual))
     
     def setUp(self):
         wr("\x1bc")
@@ -45,98 +52,93 @@ class TestCSI(unittest.TestCase):
 
     def test_insert_blank(self):
         wr("\x1b[1;1H\x1b[4@")
-        o = strw(self.orig, 1, 1, "    ").strip()
-        r = readscreen().strip()
-        self.assertEqual(o, r, "\n%s\n!=\n%s" % (o, r))
+        o = strw(self.orig, 1, 1, "    ")
+        r = readscreen()
+        self.assertScreen(o, r)
 
     def test_cursor_relative(self):
         wr("\x1b[2;2H\x1b[3B\x1b[1A\x1b[3C\x1b[1DX")
-        o = strw(self.orig, 4, 4, "X").strip()
-        r = readscreen().strip()
-        self.assertEqual(o, r, "\n%s\n!=\n%s" % (o, r))
+        o = strw(self.orig, 4, 4, "X")
+        r = readscreen()
+        self.assertScreen(o, r)
 
     def test_cursor_next_prev_line(self):
         wr("\x1b[3;9H\x1b[1EX")
         wr("\x1b[3;9H\x1b[1FY")
-        o = strw(strw(self.orig, 1, 4, "X"), 1, 2, "Y").strip()
-        r = readscreen().strip()
-        self.assertEqual(o, r, "\n%s\n!=\n%s" % (o, r))
+        o = strw(strw(self.orig, 1, 4, "X"), 1, 2, "Y")
+        r = readscreen()
+        self.assertScreen(o, r)
         
     def test_cursor_char_absolute(self):
         wr("\x1b[3;9H\x1b[4GX")
-        o = strw(self.orig, 4, 3, "X").strip()
-        r = readscreen().strip()
-        self.assertEqual(o, r, "\n%s\n!=\n%s" % (o, r))
+        o = strw(self.orig, 4, 3, "X")
+        r = readscreen()
+        self.assertScreen(o, r)
         
     def test_cursor_absolute(self):
         wr("\x1b[3;9HX")
-        o = strw(self.orig, 9, 3, "X").strip()
-        r = readscreen().strip()
-        self.assertEqual(o, r, "\n%s\n!=\n%s" % (o, r))
+        o = strw(self.orig, 9, 3, "X")
+        r = readscreen()
+        self.assertScreen(o, r)
 
     def test_cursor_tab_forward(self):
         wr("\x1b[3;1H\x1b[2IX")
-        o = strw(self.orig, 17, 3, "X").strip()
-        r = readscreen().strip()
-        self.assertEqual(o, r, "\n%s\n!=\n%s" % (o, r))
+        o = strw(self.orig, 17, 3, "X")
+        r = readscreen()
+        self.assertScreen(o, r)
 
     def test_clear_below_cursor(self):
         wr("\x1b[3;1H\x1b[0J")
-        o = "\n".join(self.orig.split("\n")[:2]).strip()
-        r = readscreen().strip()
-        self.assertEqual(o, r, "\n%s\n!=\n%s" % (o, r))
+        o = "\n".join(self.orig.split("\n")[:2])
+        r = readscreen()
+        self.assertScreen(o, r)
 
     def test_clear_above_cursor(self):
         wr("\x1b[3;1H\x1b[1J")
-        o = "\n".join(self.orig.split("\n")[2:]).strip()
-        r = readscreen().strip()
-        self.assertEqual(o, r, "\n%s\n!=\n%s" % (o, r))
+        o = "\n".join([" " * self.linelen, " " * self.linelen] + self.orig.split("\n")[2:])
+        r = readscreen()
+        self.assertScreen(o, r)
 
     def test_clear_to_end_of_line(self):
         wr("\x1b[3;9HY\x1b[3;8H\x1b[0KX")
         o = strw(self.orig, self.linelen, 3, " ")
         o = strw(o, 9, 3, " ")
-        o = strw(o, 8, 3, "X").strip()
-        o = o.strip()
-        r = readscreen().strip()
-        self.assertEqual(o, r, "\n%s\n!=\n%s" % (o, r))
+        o = strw(o, 8, 3, "X")
+        r = readscreen()
+        self.assertScreen(o, r)
 
     def test_clear_to_beginning_of_line(self):
         wr("\x1b[3;8HY\x1b[3;9H\x1b[1KX")
         o = strw(self.orig, 1, 3, " ")
         o = strw(o, 8, 3, " ")
-        o = strw(o, 9, 3, "X").strip()
-        o = o.strip()
-        r = readscreen().strip()
-        self.assertEqual(o, r, "\n%s\n!=\n%s" % (o, r))
+        o = strw(o, 9, 3, "X")
+        r = readscreen()
+        self.assertScreen(o, r)
 
     def test_clear_line(self):
         wr("\x1b[3;8HX\x1b[2KX")
         o = strw(self.orig, 1, 3, " ")
         o = strw(o, self.linelen, 3, " ")
         o = strw(o, 8, 3, " ")
-        o = strw(o, 9, 3, "X").strip()
-        o = o.strip()
-        r = readscreen().strip()
-        self.assertEqual(o, r, "\n%s\n!=\n%s" % (o, r))
+        o = strw(o, 9, 3, "X")
+        r = readscreen()
+        self.assertScreen(o, r)
 
     def test_insert_lines(self):
         wr("\x1b[3;9H\x1b[2LX")
         lines = self.orig.split("\n")
         o = "\n".join(lines[:2] + [" " * self.linelen, " " * self.linelen] + lines[2:])
         o = strw(o, 9, 3, "X")
-        o = o.strip()
-        r = readscreen().strip()
-        self.assertEqual(o, r, "\n%s\n!=\n%s" % (o, r))
+        r = readscreen()
+        self.assertScreen(o, r)
 
     def test_delete_lines(self):
-        wr("\x1b[4;9HA\x1b[3;9HB\x1b[1MX")
+        wr("\x1b[4;9HA\x1b[3;9HB\x1b[3;9H\x1b[1MX")
         lines = self.orig.split("\n")
         o = "\n".join(lines[:2] + lines[3:])
         o = strw(o, 9, 3, "X")
-        o = o.strip()
-        r = readscreen().strip()
-        self.assertEqual(o, r, "\n%s\n!=\n%s" % (o, r))
+        r = readscreen()
+        self.assertScreen(o, r)
 
     def test_delete_characters(self):
         wr("\x1b[3;9HABCD\x1b[3;9H\x1b[2PX")
@@ -145,37 +147,32 @@ class TestCSI(unittest.TestCase):
         o = strw(self.orig, 9, 3, "XD")
         o = strw(o, 30, 3, "|")
         o = strw(o, self.linelen, 3, " ")
-        o = o.strip()
-        r = readscreen().strip()
-        self.assertEqual(o, r, "\n%s\n!=\n%s" % (o, r))
+        r = readscreen()
+        self.assertScreen(o, r)
 
     def test_scroll_up(self):
         wr("\x1b[2S")
         lines = self.orig.strip().split("\n")
         o = "\n".join(lines[2:])
-        o = o.strip()
-        r = readscreen().strip()
-        self.assertEqual(o, r, "\n%s\n!=\n%s" % (o, r))
+        r = readscreen()
+        self.assertScreen(o, r)
 
     def test_scroll_down(self):
-        wr("\x1b[2T")
-        lines = self.orig.strip().split("\n")
-        o = "\n".join(lines[:-2])
-        o = o.strip()
-        r = readscreen().strip()
-        self.assertEqual(o, r, "\n%s\n!=\n%s" % (o, r))
+        wr("\x1b[2T\x1b[1;1HX")
+        o = "X\n\n" + self.orig
+        r = readscreen()
+        self.assertScreen(o, r)
 
     def test_erase_character(self):
         wr("\x1b[3;8HABCD\x1b[3;8H\x1b[3XY")
         o = strw(self.orig, 8, 3, "Y")
         o = strw(o, 11, 3, "D")
-        o = o.strip()
-        r = readscreen().strip()
-        self.assertEqual(o, r, "\n%s\n!=\n%s" % (o, r))
+        r = readscreen()
+        self.assertScreen(o, r)
 
     def test_cursor_tab_backward(self):
         wr("\x1b[3;17HA\x1b[1ZB")
         o = strw(self.orig, 17, 3, "A")
-        o = strw(o, 9, 3, "B").strip()
-        r = readscreen().strip()
-        self.assertEqual(o, r, "\n%s\n!=\n%s" % (o, r))
+        o = strw(o, 9, 3, "B")
+        r = readscreen()
+        self.assertScreen(o, r)
