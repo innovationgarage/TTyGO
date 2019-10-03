62c3a3b41e3d26728f2502b221d83fc553c17521
Egil <egil.moller@freecode.no>
2019-10-03 15:55:26 +0200

    Automated build script

---
+++ install.sh
@@ -0,0 +1,16 @@
+[ "$TTGOTERM" ] || TTGOTERM=/dev/ttyUSB0
+
+if ! [ -e ~/bin/arduino-cli ]; then
+  (
+    cd ~
+    curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | sh
+  )
+fi
+
+arduino-cli core update-index --additional-urls http://arduino.esp8266.com/stable/package_esp8266com_index.json
+arduino-cli core install esp8266:esp8266 --additional-urls http://arduino.esp8266.com/stable/package_esp8266com_index.json
+
+arduino-cli lib install OneButton U8g2 ESP8266Scheduler
+
+arduino-cli compile --fqbn esp8266:esp8266:nodemcu TTyGO -v
+arduino-cli upload -p $TTGOTERM --fqbn esp8266:esp8266:nodemcu TTyGO
