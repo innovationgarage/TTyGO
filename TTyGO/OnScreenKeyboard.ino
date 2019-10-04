56face10df7e7aa97986b51b959c1c6ac2668276
Erwin Ried <1091420+eried@users.noreply.github.com>
2019-10-04 16:14:37 +0200

    Update OnScreenKeyboard.ino

--- TTyGO/OnScreenKeyboard.ino
+++ TTyGO/OnScreenKeyboard.ino
@@ -6,7 +6,15 @@ typedef struct {
   char code[8];
 } Key;
 
-const Key osk_keyboard[] = { { "A", "A", }, { "B", "B", }, { "C", "C", }, { "RET", '\n', }, { "ESC", 11, }, { "TAB", '\t', } };
+const Key osk_keyboard[] = { {"A", "A"}, {"B", "B"}, {"C", "C"}, {"D", "D"}, {"E", "E"}, {"F", "F"}, {"G", "G"}, {"H", "H"}, {"I", "I"}, {"J", "J"}, {"K", "K"},
+  {"L", "L"}, {"M", "M"}, {"N", "N"}, {"O", "O"}, {"P", "P"}, {"Q", "Q"}, {"R", "R"}, {"S", "S"}, {"T", "T"}, {"U", "U"}, {"V", "V"}, {"W", "W"}, {"X", "X"},
+  {"Y", "Y"}, {"Z", "Z"}, {"0", "0"}, {"1", "1"}, {"2", "2"}, {"3", "3"}, {"4", "4"}, {"5", "5"}, {"6", "6"}, {"7", "7"}, {"8", "8"}, {"9", "9"}, {"a", "a"},
+  {"b", "b"}, {"c", "c"}, {"d", "d"}, {"e", "e"}, {"f", "f"}, {"g", "g"}, {"h", "h"}, {"i", "i"}, {"j", "j"}, {"k", "k"}, {"l", "l"}, {"m", "m"}, {"n", "n"},
+  {"o", "o"}, {"p", "p"}, {"q", "q"}, {"r", "r"}, {"s", "s"}, {"t", "t"}, {"u", "u"}, {"v", "v"}, {"w", "w"}, {"x", "x"}, {"y", "y"}, {"z", "z"}, {"[", "["},
+  {"]", "]"}, {"(", "("}, {")", ")"}, {".", "."}, {",", ","}, {":", ":"}, {";", ";"}, {"+", "+"}, {"-", "-"}, {"*", "*"}, {"/", "/"}, {"!", "!"}, {""", """},
+  {"#", "#"}, {"%", "%"}, {"&", "&"}, {"?", "?"}, {"'", "'"}, {"*", "*"}, {"|", "|"}, {"_", "_"}, {"<", "<"}, {">", ">"}, {"ESC", ESC}, {"RET", "\n"}, {"TAB", "\t"},
+};
+
 const int osk_keyboard_length = ArrayLength(osk_keyboard);
 const int osk_default_selection = 0;
 const int osk_largejump = 10; // When holding a key
@@ -15,9 +23,11 @@ int osk_current_selection;
 // Draws the on screen keyboard to the lcd
 void osk_draw()
 {
-  u8g2.drawRBox(0,0,50,50,3);
-  u8g2.setCursor(5,40);
+  u8g2.drawRBox(10, 10, 17, 10, 3);
+  u8g2.setCursor(12, 17);
+  u8g2.setDrawColor(0);
   u8g2.print(osk_keyboard[osk_current_selection].label);
+  u8g2.setDrawColor(1);
 }
 
 // Returns true if it was out of bounds
